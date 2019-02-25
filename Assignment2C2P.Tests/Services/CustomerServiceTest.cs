e082604767e44a4853c0a24db222aaaaaa1bbd9b
S. Chaipetch <supitch@gmail.com>
2019-02-25 15:07:26 +0700

    Created CustomerService class and implemented GetCustomerById method

---
+++ Assignment2C2P.Tests/Services/CustomerServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Assignment2C2P.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Assignment2C2P.Models;
+using Moq;
+using Moq.EntityFrameworkCore;
+using Xunit;
+
+namespace Assignment2C2P.Tests.Services
+{
+    public class CustomerServiceTest
+    {
+        private IList<Customer> customers;
+
+        public CustomerServiceTest()
+        {
+            customers = new List<Customer>
+            {
+                new Customer {CustomerId = 1, Name = "Customer 1", Email = "customer1@mail.com", MobileNo = "0891234567"},
+                new Customer {CustomerId = 2, Name = "Customer 2", Email = "customer2@mail.com", MobileNo = "0891234568"},
+                new Customer {CustomerId = 3, Name = "Customer 3", Email = "customer3@mail.com", MobileNo = "0891234569"},
+            };
+        }
+
+        [Fact]
+        public void GetCustomerById_IdNotFound_MustReturnNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<Assignment2C2PContext>();
+
+            dbContextMock.Setup(x => x.Customer).ReturnsDbSet(customers);
+
+            CustomerService service = new CustomerService(dbContextMock.Object);
+
+            //Act
+            var actual = service.GetCustomerById(0);
+
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void GetCustomerById_ValidId_MustReturnCustomer()
+        {
+            //Arrange
+            var dbContextMock = new Mock<Assignment2C2PContext>();
+
+            dbContextMock.Setup(x => x.Customer).ReturnsDbSet(customers);
+
+            CustomerService service = new CustomerService(dbContextMock.Object);
+
+            //Act
+            var actual = service.GetCustomerById(2);
+            var expected = customers[1];
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Assignment2C2P/Services/CustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Assignment2C2P.Models;
+
+namespace Assignment2C2P.Services
+{
+    public class CustomerService
+    {
+        private readonly Assignment2C2PContext _db;
+
+        public CustomerService(Assignment2C2PContext db)
+        {
+            _db = db;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _db.Customer.FirstOrDefault(c => c.CustomerId == id);
+        }
+    }
+}
