89b3874919f5a2c66a0c99aa8966ebd981bc0964
S. Chaipetch <supitch@gmail.com>
2019-02-26 15:48:25 +0700

    Implemented InquiryCriteriaValidation

---
+++ Assignment2C2P.Tests/Validation/InquiryCriteriaValidationTest.cs
@@ -0,0 +1,99 @@
+﻿using Assignment2C2P.Validation;
+using Xunit;
+
+namespace Assignment2C2P.Tests.Validation
+{
+    public class InquiryCriteriaValidationTest
+    {
+        [Fact]
+        public void ValidateCustomerID_LengthWasExceed_ReturnsFalse()
+        {
+            //Arrange
+            string invalidCustomerId = "12345678901";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateCustomerID(invalidCustomerId);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ValidateCustomerID_NoneNumericCriteria_ReturnsFalse()
+        {
+            //Arrange
+            string invalidCustomerId = "ID1234";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateCustomerID(invalidCustomerId);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ValidateCustomerID_ZeroCustomerIdCriteria_ReturnsFalse()
+        {
+            //Arrange
+            string invalidCustomerId = "-1";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateCustomerID(invalidCustomerId);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ValidateCustomerID_CustomerIdCriteriaLessThanZero_ReturnsFalse()
+        {
+            //Arrange
+            string invalidCustomerId = "-1";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateCustomerID(invalidCustomerId);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ValidateCustomerID_ValidCriteria_ReturnsTrue()
+        {
+            //Arrange
+            string validCustomerId = "123456";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateCustomerID(validCustomerId);
+
+            //Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ValidateEmail_InvalidEmail_ReturnsFalse()
+        {
+            //Arrange
+            string invalidEmail = "invalid@email";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateEmail(invalidEmail);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ValidateEmail_ValidEmail_ReturnsTrue()
+        {
+            //Arrange
+            string validEmail = "valid@email.com";
+
+            //Act
+            var actual = InquiryCriteriaValidation.ValidateEmail(validEmail);
+
+            //Assert
+            Assert.True(actual);
+        }
+    }
+}
---
+++ Assignment2C2P/Validation/InquiryCriteriaValidation.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using NJsonSchema.Validation.FormatValidators;
+
+namespace Assignment2C2P.Validation
+{
+    public class InquiryCriteriaValidation
+    {
+        public static bool ValidateCustomerID(string customerIdCriteria)
+        {
+            if (string.IsNullOrEmpty(customerIdCriteria)) return false;
+
+            //Max length 10 digits
+            if (customerIdCriteria.Length > 10) return false;
+
+            //Check if it could be parsed to number
+            if (!int.TryParse(customerIdCriteria, out int customerId)) return false;
+
+            //Check if customer id is valid
+            return customerId > 0;
+        }
+
+        public static bool ValidateEmail(string email)
+        {
+            EmailFormatValidator validator = new EmailFormatValidator();
+            return validator.IsValid(email, JTokenType.None);
+        }
+    }
+}
